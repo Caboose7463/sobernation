599974a5dc6f89f78aac31b1ac8b660a4b237055
PX Deploy <deploy@px.app>
2026-04-08 09:00:37 +0100

    fix: correct import path in go/[type]/[slug]/route.ts (../../../ -> ../../../../lib/ads)

--- app/go/[type]/[slug]/route.ts
+++ app/go/[type]/[slug]/route.ts
@@ -11,7 +11,7 @@
  */
 
 import { NextRequest, NextResponse } from 'next/server'
-import { processClick } from '../../../lib/ads'
+import { processClick } from '../../../../lib/ads'
 
 export const dynamic = 'force-dynamic'
 
